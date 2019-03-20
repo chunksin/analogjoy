daadf24c5c5b3ace18ab85ea90f60992da3ee41a
chunksin <chunksinspace@gmail.com>
2019-03-20 09:18:05 +0000

    Create myconfig.py

---
+++ myconfig.py
@@ -0,0 +1,13 @@
+# myconfig.py:
+joy1_x_channel=0
+joy1_y_channel=1
+joy1_z_channel=2
+joy2_x_channel=3
+joy2_y_channel=4
+joy2_z_channel=5
+deadzone=0
+fuzz=0
+toggleshift=1
+shifterpin=23
+mode=1
+enabled=1
