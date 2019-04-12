bf3ab36e459c49e60152e63b97d91e332a83d825
chunksin <chunksinspace@gmail.com>
2019-04-12 13:42:33 +0100

    Update analogmulti.py

--- analogmulti.py
+++ analogmulti.py
@@ -17,22 +17,63 @@ if enabled==0:
   sys.exit()
 
 # Function for toggle shifter button press
-def button_callback(channel):
-    if GPIO.input(shifterpin):
-      device.emit(uinput.BTN_JOYSTICK,1)
-      time.sleep(0.1)
-      device.emit(uinput.BTN_JOYSTICK,0)
-    else:
-      device.emit(uinput.BTN_JOYSTICK,1)
-      time.sleep(0.1)
-      device.emit(uinput.BTN_JOYSTICK,0)
+def shifter_callback(channel):
+     device.emit(uinput.BTN_JOYSTICK,1)
+     time.sleep(0.1)
+     device.emit(uinput.BTN_JOYSTICK,0)
 
-# Initialise GPIO for button
-if driver and toggleshift:
+# Function for player 1 start button press
+def p1start_callback(channel):
+   if GPIO.input(p1b1pin):
+     device1.emit(uinput.BTN_START,1)
+   else:
+     device1.emit(uinput.BTN_START,0)
+
+# Function for player 2 start button press
+def p2start_callback(channel):
+   if GPIO.input(p2startpin):
+     device2.emit(uinput.BTN_START,1)
+   else:
+     device2.emit(uinput.BTN_START,0)
+
+# Function for player 1 button 1 press
+def p1b1_callback(channel):
+   if GPIO.input(p1b1pin):
+     device1.emit(uinput.BTN_1,1)
+   else:
+     device1.emit(uinput.BTN_1,0)
+
+# Function for player 2 button 1 press
+def p2b1_callback(channel):
+   if GPIO.input(p2b1pin):
+     device2.emit(uinput.BTN_1,1)
+   else:
+     device2.emit(uinput.BTN_1,0)
+
+# Initialise GPIO for shifter button
+if mode==1 and toggleshift==1:
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setup(shifterpin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-	# Add interrupt for button pin detection
-	GPIO.add_event_detect(shifterpin, GPIO.BOTH, callback=button_callback, bouncetime=300)
+	# Add interrupt for shifter button pin detection
+	GPIO.add_event_detect(shifterpin, GPIO.BOTH, callback=shifter_callback, bouncetime=300)
+
+# Initialise GPIO for single joystick buttons
+if mode==2 or mode==3:
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(p1startpin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	GPIO.setup(p1b1pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	# Add interrupt for player 1 pin detection
+	GPIO.add_event_detect(p1startpin, GPIO.BOTH, callback=p1start_callback, bouncetime=300)
+    GPIO.add_event_detect(p1b1pin, GPIO.BOTH, callback=p1b1_callback, bouncetime=300)
+
+# Initialise GPIO for second joystick buttons
+if mode==3:
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(p2startpin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	GPIO.setup(p2b1pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	# Add interrupt for player 2 pin detection
+	GPIO.add_event_detect(p2startpin, GPIO.BOTH, callback=p2start_callback, bouncetime=300)
+    GPIO.add_event_detect(p2b1pin, GPIO.BOTH, callback=p2b1_callback, bouncetime=300)
 
 # Open SPI bus
 spi = spidev.SpiDev()
@@ -64,24 +105,30 @@ if mode==1:
 
 if mode==2:
 	# Set up uinput virtual device and begin polling for input
-	device = uinput.Device([uinput.BTN_JOYSTICK,
+	device1 = uinput.Device([uinput.BTN_JOYSTICK,
+                      uinput.BTN_START,
+                      uinput.BTN_1,
                       uinput.ABS_X+(0,1023,fuzz,deadzone),
                       uinput.ABS_Y+(0,1023,fuzz,deadzone)
                       ])
     while True:
        joy1_x_value=ReadChannel(joy1_x_channel)
-       device.emit(uinput.ABS_X,joy1_x_value,syn=True)
+       device1.emit(uinput.ABS_X,joy1_x_value,syn=True)
        joy1_y_value=ReadChannel(joy1_y_channel)
-       device.emit(uinput.ABS_Y,joy1_y_value,syn=True)
+       device1.emit(uinput.ABS_Y,joy1_y_value,syn=True)
        time.sleep(0.010)
 					  
 if mode==3:
 	# Set up uinput virtual devices and begin polling for input
 	device1 = uinput.Device([uinput.BTN_JOYSTICK,
+                      uinput.BTN_START,
+                      uinput.BTN_1,
                       uinput.ABS_X+(0,1023,fuzz,deadzone),
                       uinput.ABS_Y+(0,1023,fuzz,deadzone)
                       ])
 	device2 = uinput.Device([uinput.BTN_JOYSTICK,
+                      uinput.BTN_START,
+                      uinput.BTN_1,
                       uinput.ABS_X+(0,1023,fuzz,deadzone),
                       uinput.ABS_Y+(0,1023,fuzz,deadzone)
                       ])
